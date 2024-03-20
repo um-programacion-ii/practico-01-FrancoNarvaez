3e0eb4441a950289f42914f9cd1c58d714859d6a
FrancoNarvaez <f.narvaez@alumno.um.edu.ar>
2024-03-19 21:06:39 -0300

    Primer punto terminado

---
+++ entity/Ingrediente.js
@@ -0,0 +1,32 @@
+class Ingrediente {
+    protected String nombre;
+    protected int cantidad;
+
+    public Ingrediente(String nombre,int cantidad) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public int getCantidad() {
+        return cantidad;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    @Override
+    public String toString() {
+        return "Ingrediente [cantidad=" + cantidad + ", nombre=" + nombre + "]";
+    }
+}
+
+class main {
+    public static void main {
+        Ingrediente ingrediente = new Ingrediente("Azucar", 2);
+        System.out.println(ingrediente);
+    }
+}
